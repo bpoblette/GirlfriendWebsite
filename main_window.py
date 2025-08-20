471e4ebf57861aa23ae4e1a2c0bcae816de22b8d
bpoblette <bpoblette@zagmail.gonzaga.edu>
2025-08-20 14:32:54 -0700

    got PyQt6 working. Learning basics now

--- main_window.py
+++ main_window.py
@@ -1,10 +1,19 @@
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton
+
+class MainWindow(QMainWindow):
+
+  def __init__(self):
+    super().__init__()
+
+    self.setWindowTitle("My Girlfriend App")
+    button = QPushButton("Press Me!")
+    button.setCheckable(True)
+    self.setCentralWidget(button)
+
 
 app = QApplication([])
 
-window = QWidget
+window = MainWindow()
 window.show()
 
 app.exec()
-
-# Need to figure out how to use PyQt
