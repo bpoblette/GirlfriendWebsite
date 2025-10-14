69473bdc8727dfc85edf010f8fbc16aa03cbb315
bpoblette <bpoblette@zagmail.gonzaga.edu>
2025-10-14 14:40:18 -0700

    added new routes and added more html files for different pages.

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html')); 
+}); 
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'about.html'));
+}); 
+
+app.get('/photos', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'photos.html'));
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
