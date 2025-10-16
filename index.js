084477089bc4908de08258b4556b66823ae0c2ab
bpoblette <bpoblette@zagmail.gonzaga.edu>
2025-10-15 23:44:46 -0700

    figuring out how I want to position everything

--- index.js
+++ index.js
@@ -17,10 +17,6 @@ app.get('/photos', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'photos.html'));
 });
 
-app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
-});
-
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
